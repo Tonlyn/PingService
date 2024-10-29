514b7477187f25d7d8b9c94a816ddaa6b7326d6d
linshy <delightlyn@163.com>
2024-10-29 21:16:07 +0800

    add Ping Service shell

--- start-services.sh
+++ start-services.sh
@@ -7,6 +7,6 @@ java -jar target/pingService-0.0.1-SNAPSHOT.jar --spring.application.name=ping-s
 java -jar target/pingService-0.0.1-SNAPSHOT.jar --spring.application.name=ping-service-3 --server.port=8083 &
 
 # 启动第三个Ping服务实例
-java -jar target/pingSrvice-0.0.1-SNAPSHOT.jar --spring.application.name=ping-service-4 --server.port=8084 &
+java -jar target/pingService-0.0.1-SNAPSHOT.jar --spring.application.name=ping-service-4 --server.port=8084 &
 
 echo "All Ping services started."
